6bd6638698497d95f5e13c95cbcff68185bbe49c
lxcste <lxcste@gmail.com>
2025-09-19 19:08:03 -0300

    init project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -26,6 +27,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        /* eslint-disable-next-line */
+        cz-shortcut-listen="true"
       >
         {children}
       </body>
