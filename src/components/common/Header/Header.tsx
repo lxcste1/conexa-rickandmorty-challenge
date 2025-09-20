725d14483f5c8cdf3a5374827faf7b48a5d6a7da
lxcste <lxcste@gmail.com>
2025-09-20 01:07:23 -0300

    feat: add header and footer components

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+function Header() {
+  const path = usePathname();
+  const isComparePage = path === "/compare";
+
+  return (
+    <header className="bg-accent py-4 shadow-md">
+      <div className="flex justify-center items-center max-w-5xl mx-auto">
+        <Link className="flex flex-col items-center" href="/">
+          <Image
+            src="/rickandmorty-logo.png"
+            alt="logo"
+            width={230}
+            height={70}
+          />
+          <span className="font-bold text-sm">Conexa Challenge</span>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
